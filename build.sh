4ae3ca10ed73e1c3414a7fa3dd1d583c7476c6ae
Benjamin Cabé <BCabe@sierrawireless.com>
2012-08-29 16:42:37 +0200

    wip

---
+++ build.sh
@@ -0,0 +1,9 @@
+while true; do
+
+make
+cp docs/assets/js/bootstrap.min.js /Users/kartben/Repositories/m2m.eclipse.org/js/bootstrap
+cp docs/assets/css/bootstrap.css /Users/kartben/Repositories/m2m.eclipse.org/css/bootstrap/bootstrap.min.css
+cp docs/assets/css/bootstrap-responsive.css /Users/kartben/Repositories/m2m.eclipse.org/css/bootstrap/bootstrap-responsive.min.css
+sleep 2
+
+done
